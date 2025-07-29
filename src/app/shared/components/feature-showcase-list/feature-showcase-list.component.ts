12ac25668efb24d719f068130e377d330dca1624
Leonardo Valeriano <leonardo.valeriano.public@gmail.com>
2025-07-28 23:56:44 -0500

    remove code highlight service

--- src/app/shared/components/feature-showcase-list/feature-showcase-list.component.ts
+++ src/app/shared/components/feature-showcase-list/feature-showcase-list.component.ts
@@ -4,7 +4,6 @@ import { features } from '../../data';
 import { CommonModule, NgComponentOutlet } from '@angular/common';
 import { FeatureExampleComponent } from '../feature-example/feature-example.component';
 import { FeatureFileListComponent } from '../feature-file-list/feature-file-list.component';
-import { CodeHighlightService } from '../../services/code-highlight.service';
 
 @Component({
   selector: 'app-feature-showcase-list',
@@ -21,32 +20,6 @@ import { CodeHighlightService } from '../../services/code-highlight.service';
 })
 export class FeatureShowcaseListComponent {
   featureList = signal<typeof features>(features);
-
-  constructor(private codeHighlightService: CodeHighlightService) {}
-
-  ngOnInit() {
-    this.angularHtmlToHtml();
-  }
-
-  async angularHtmlToHtml() {
-    const newFeatureList = await Promise.all(
-      [...this.featureList()].map(async (feature) => {
-        const processedFiles = await Promise.all(
-          feature.files.map(async (file) => {
-            const html = await this.codeHighlightService.angularHtmlToHtml(
-              file.code
-            );
-            return {
-              ...file,
-              prettyCode: html,
-            };
-          })
-        );
-        return { ...feature, files: processedFiles };
-      })
-    );
-    this.featureList.set(newFeatureList);
-  }
 
   trackByFeatureName(index: number, item: (typeof features)[0]): string {
     return item.title;
